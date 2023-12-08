7abf157fb2343a888707b9cf46b07ef6fdf5e0e7
dwidge <dwidge+gh@gmail.com>
2023-12-08 12:33:46 +0200

    Add cond parameter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-export const maskKeys = (keys: string[], object: object): object =>
-  Object.fromEntries(
-    Object.entries(object).map(([k, v]) =>
-      typeof v === "object" && v !== null
-        ? [k, maskKeys(keys, v)]
-        : keys.some((key) => k.toLowerCase().includes(key))
+export const maskKeys =
+  (cond: (key: string) => boolean) =>
+  <T extends object>(object: T): T =>
+    Object.fromEntries(
+      Object.entries(object).map(([k, v]) =>
+        typeof v === "object" && v !== null
+          ? [k, maskKeys(cond)(v)]
+          : cond(k)
           ? [k, maskValue(v)]
           : [k, v]
-    )
-  );
+      )
+    ) as T;
 
-export const maskValue = (v: any) =>
+export const maskValue = <T>(v: T) =>
   v == null ? v : "*".repeat(v?.toString?.().length ?? 1);
+
+export const includesAny = (items: string[]) => (value: string) =>
+  items.some((key) => value.toLowerCase().includes(key));
