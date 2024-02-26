bbcb5d0682b06ab82798ab43577a2eb7b0ca18bb
dwidge <dwidge+gh@gmail.com>
2024-02-26 11:00:31 +0200

    Handle arrays

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ export const maskKeys =
   (cond: (key: string) => boolean) =>
   <T extends object>(object: T): T =>
     Object.fromEntries(
-      Object.entries(object).map(([k, v]) =>
-        typeof v === "object" && v !== null
-          ? [k, maskKeys(cond)(v)]
-          : cond(k)
-          ? [k, maskValue(v)]
-          : [k, v]
-      )
+      Object.entries(object).map(([k, v]) => {
+        if (Array.isArray(v)) {
+          // Iterate arrays
+          return [k, v.map((v) => maskKeys(cond)(v))];
+        } else if (typeof v === "object" && v !== null) {
+          // Recurse nested objects
+          return [k, maskKeys(cond)(v)];
+        } else {
+          // Apply masking condition to other values
+          return cond(k) ? [k, maskValue(v)] : [k, v];
+        }
+      })
     ) as T;
 
 export const maskValue = <T>(v: T) =>
